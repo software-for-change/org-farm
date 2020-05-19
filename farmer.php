e5729e955f46249d853352577203f6c5d70bda4f
Nekesa Mercy <kesamercy@gmail.com>
2020-05-18 22:58:00 -0400

    added input fields  to allow a farmer to enter the types of food they would like to inventory

---
+++ farmer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Simply Organic</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500&family=Noto+Serif:wght@700&family=Roboto+Slab:wght@900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&family=Fredericka+the+Great&family=Noto+Serif&family=Roboto&display=swap" rel="stylesheet">
+    <title>Farmer Inventory</title>
+</head>
+
+<body>
+
+    <h1> Faremr's Inventory </h1>
+
+    <select id="inputs" style="width:60px;">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>                      
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+        <option>10</option>
+    </select>
+
+    <button onclick="window.location.href = 'customer.php';">view as a customer </button>
+    <button onclick="window.location.href = 'farmer.php';">access as a farmer</button>
+    
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="index.js"></script>
+    <script>
+        $(document).ready(function() {
+
+
+        });
+    </script>
+
+</body>
+
+</html>
