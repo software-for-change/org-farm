6883cd5a2dd793e75281e75ae7b4b4e69979f1cd
Nekesa Mercy <kesamercy@gmail.com>
2020-05-20 13:07:58 -0400

    #2 implemented login for the faremer

--- farmer/farmer.php
+++ farmer/farmer.php
@@ -12,7 +12,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Simply Organic</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="./style.css" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500&family=Noto+Serif:wght@700&family=Roboto+Slab:wght@900&display=swap"
@@ -26,13 +26,6 @@
 <body>
 
     <h1> Farmer's Inventory </h1>
-
-    <label for="inputs"> Enter how many types of food you want to inventory.
-    </label>
-    <p>Hold down the Ctrl (windows) / Command (Mac) button to select multiple options.</p>
-    <br><br>
-   
-
 
     <h1>enter the quantity for the foods you wish to add</h1>
     <?php
