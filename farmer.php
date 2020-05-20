66444f8e47d5d26e472491a64a681bbc5527b38d
Nekesa Mercy <kesamercy@gmail.com>
2020-05-20 11:31:58 -0400

    debugging the produce table-3

--- farmer.php
+++ farmer.php
@@ -13,7 +13,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Simply Organic</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <script type="text/javascript" src="js/modernizr.custom.86080.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link
         href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500&family=Noto+Serif:wght@700&family=Roboto+Slab:wght@900&display=swap"
@@ -56,9 +55,10 @@
         if ($result->num_rows > 0) {
             echo "<form>";
             echo "<table class='prodcue-table'><tr style='height: 80px'><th style='text-align:left'> Food Type </th><th style='text-align:left'> Quantity </th></tr><br><br>";
+            echo "<p>did we get here</p>";
             // output data of each row
-            while($row = $result->fetch_assoc()) {
-                echo "<p>did we get here</p>";
+            while($row = mysqli_fetch_array($result)) {
+               
                 echo "<tr style='height: 40px'>
                     <td>" .$row["food_type"]. "</td>
                     <td> <input type='text'></td>
