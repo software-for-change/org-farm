d83c6df8984bfd4cbbe472f60137b2f47215e3c4
Nekesa Mercy <kesamercy@gmail.com>
2020-05-23 09:30:53 -0400

    added navigation elements and created new pages for navigation

--- farmer/farmer.php
+++ farmer/farmer.php
@@ -19,21 +19,18 @@ if (count($_POST) > 0) {
     $query = "SELECT food_id FROM farm_food WHERE food_name='$food'";
     $id_result = mysqli_query($conn, $query);
 
-
     //update the stock table with the values of the food id, the stock qnautity and the userid of the farmer
     if ($row = mysqli_fetch_assoc($id_result)) {
         $food_id = $row['food_id'];
 
         //store the user id in the userid col for the stock table
-        if(mysqli_query($conn, "INSERT INTO farm_user_stock (user_id, food_id, stock_quantity) VALUES ('$user_id', '$food_id', '$quantity')")){
+        if (mysqli_query($conn, "INSERT INTO farm_user_stock (user_id, food_id, stock_quantity) VALUES ('$user_id', '$food_id', '$quantity')")) {
             echo 'your stock has been added to the inventory';
-             
-        }
-        else {
+
+        } else {
             echo 'there was an error, your stock has not been added';
         }
 
-        
     }
 
     //note: you don't need another id in the farmers table since you use the farmer_id to store the food in the stock table.
@@ -62,34 +59,51 @@ if (count($_POST) > 0) {
 
 <body>
 
+    <div class="header">
+        <div class="menu_navbar">
+            <ul>
+                <li><a class="navlink" href="about.html">about</a> </li>
+                <li><a class="navlink" href="./admin/admin-login.php">admin login</a> </li>
+                <li><a class="navlink" href="./farmer/farmer-login.php">farmer login</a> </li>
+                <li><a class="navlink" href="./customer/customer-login.php">customer login </a> </li>
+                <li><a class="navlink" href="index.html">logout</a> </li>
+                
+            </ul>
+        </div>
+
+        <div class="logo">
+            <h2 class="logo"> <a href="index.html">Farm Organic</a> </h2>
+        </div>
+    </div>
+
     <h1> Farmer's Inventory </h1>
 
     <h1>enter the quantity for the foods you wish to add</h1>
     <?php
 
-if ($result->num_rows > 0) {
-    echo "<form method='post' action=''>";
-    echo "<table class='prodcue-table'><tr style='height: 80px'><th style='text-align:left'> Food Type </th><th style='text-align:left'> Quantity </th></tr><br><br>";
+    if ($result->num_rows > 0) {
+        echo "<form method='post' action=''>";
+        echo "<table class='prodcue-table'><tr style='height: 80px'><th style='text-align:left'> Food Type </th><th style='text-align:left'> Quantity </th></tr><br><br>";
 
-    // output data of each row
-    while ($row = mysqli_fetch_array($result)) {
+        // output data of each row
+        while ($row = mysqli_fetch_array($result)) {
 
-        $food_name = $row["food_name"];
-        echo "<tr style='height: 40px'>
-                    <td>" . $food_name . "</td>
-                    <td> <input name='quantity' type='text'></td>
-                    <input name='food_name'  type='hidden' value='$food_name' >
-                    <td> <input type='submit' value='submit'></td>
-                </tr>";
+            $food_name = $row["food_name"];
+            echo "<tr style='height: 40px'>
+                        <td>" . $food_name . "</td>
+                        <td> <input name='quantity' type='text'></td>
+                        <input name='food_name'  type='hidden' value='$food_name' >
+                        <td> <input type='submit' value='submit'></td>
+                    </tr>";
+        }
+        echo "</table>";
+
+        echo "</form>";
+
+    } else {
+        echo "0 results";
     }
-    echo "</table>";
-
-    echo "</form>";
-
-} else {
-    echo "0 results";
-}
-?>
+    ?>
 
 
 
