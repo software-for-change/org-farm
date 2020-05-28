9c706b8739eecb6453d5c667d0d75f39cfe4c8d5
Nekesa Mercy <kesamercy@gmail.com>
2020-05-28 14:49:46 -0400

    #77 added a php style sheet for styling the pages after login debugging

--- index.php
+++ index.php
@@ -7,10 +7,8 @@ $result = $conn->query($sql);
 
 if (isset($_SESSION['user_id'])) {
     echo "<link rel='stylesheet' type='text/css' media='screen' href='style.php'>";
-    echo '<body style="background-color:white; color:black; h1:white">';
-
 } else {
-    echo '<body style="background-color:#303133; ">';
+   // echo '<body style="background-color:#303133; ">';
 }
 
 ?>
