bb4c8aa07e12f60d7a070775e06efbe79d010a9b
Nekesa Mercy <kesamercy@gmail.com>
2020-05-28 17:15:20 -0400

    #77 styled the elements of after login page using in-line stling debugv3

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $result = $conn->query($sql);
 if (isset($_SESSION['user_id'])) {
     //echo "<link rel='stylesheet' type='text/css' media='screen' href='style.php'>";
     echo '<body style="background-color:white; color:black ">';
-    echo '<a style="color:black">';
+    // echo '<a style="color:black">';
     echo '<h1 style="color:white">';
 } else {
    // echo '<body style="background-color:#303133; ">';
