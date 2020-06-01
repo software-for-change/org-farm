a7c75d6636d2d2114a570ff6b47b38e59347d64e
Nekesa Mercy <kesamercy@gmail.com>
2020-06-01 12:18:47 -0400

    #57 added the php for the sign up page-debuggingv2

--- admin/admin-signup-auth.php
+++ admin/admin-signup-auth.php
@@ -1,6 +1,4 @@
 <?php
-$message = "";
-
 include_once "../access-db.php";
 
 if (count($_POST) > 0) {
@@ -14,22 +12,23 @@ if (count($_POST) > 0) {
     $count = mysqli_num_rows($result);
 
     if (empty($fname) || empty($lname)) {
-        $message = "Please enter a first and last name.";
+        echo "Please enter a first and last name.";
     } else if ((strpos($email, '@.') === false)) {
-        $message = "Please enter a valid email address.";
+        echo "Please enter a valid email address.";
     } else if ($count > 0) {
-        $message = "Email address is already in use.";
+        echo "Email address is already in use.";
     } else if (!preg_match('(^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$)', $pass)) {
-        $message = "Please enter a valid password.";
+        echo "Please enter a valid password.";
     } else if (strlen($phone) != 10) {
-        $message = "Please input a 10 digit phone number.";
+        echo "Please input a 10 digit phone number.";
     } else {
 
         if (mysqli_query($conn, "INSERT INTO farm_admin (first_name, last_name, phone, admin_email, admin_password) VALUES ('$fname', '$lname', '$phone', '$email', '$pass')")) {
-            $message = "you have successfully created an account as the admin. You can now use your credentials to login";
-
+            echo "you have successfully created an account as the admin. You can now use your credentials to login";
+            header("location:admin-login.php?");
         } else {
-            $message = 'sorry your account has not been created';
+            echo 'sorry your account has not been created';
+            header("location:admin-login.php?");
         }
     }
 }
