b842da70e6a4c14a301cbc5ee59157eeb4038d44
Nekesa Mercy <kesamercy@gmail.com>
2020-06-08 14:16:28 -0400

    #91 added the contact us page and modified the about page

--- contact.php
+++ contact.php
@@ -40,7 +40,31 @@
         <p>Simply Organic</p>
     </div>
 
-    <p> this is the contnet for the about me page</p>
+    <h3>Contact Form</h3>
+
+    <h4>Contact us business@gmail.com</h4>
+
+<div class="container">
+  <form action="/action_page.php">
+    <label for="fname">First Name</label>
+    <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+    <label for="lname">Last Name</label>
+    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+    <label for="country">Country</label>
+    <select id="country" name="country">
+      <option value="australia">Australia</option>
+      <option value="canada">Canada</option>
+      <option value="usa">USA</option>
+    </select>
+
+    <label for="subject">Subject</label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+</div>
 
     <br> <br>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
