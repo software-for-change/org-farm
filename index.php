44a2d2e3921797fff0c85e4c00bf711f867ed8dc
Nekesa Mercy <kesamercy@gmail.com>
2020-06-12 10:37:20 -0400

    #46 redesign of UI for index page

--- index.php
+++ index.php
@@ -174,8 +174,11 @@ if ($result->num_rows > 0) {
                     <hr class="bar second-bar">
 
                     <ul>
+                    <img src="https://img.icons8.com/color/48/000000/beet.png"/>
                         <li>we get our fresh produce from our organic farms</li>
+                        <img src="https://img.icons8.com/color/48/000000/beet.png"/>
                         <li>You create an account and choose your delivery frequency</li>
+                        <img src="https://img.icons8.com/color/48/000000/beet.png"/>
                         <li>select your box size</li>
                         <li>We then deliver our simpliy organic products to your door step </li>
                     </ul>
