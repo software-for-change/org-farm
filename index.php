09ba115797edd176b850e95cf645e2c950e74d2c
Nekesa Mercy <kesamercy@gmail.com>
2020-06-12 11:02:22 -0400

    #46 redesign of UI for index page

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ $result = $conn->query($sql);
         href="https://fonts.googleapis.com/css2?family=Barlow&family=Fredericka+the+Great&family=Noto+Serif&family=Roboto&display=swap"
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Enriqueta:wght@700&display=swap" rel="stylesheet">
     <title>Customer</title>
 </head>
 
