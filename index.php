8c322ea4a702f07b01093dc8b37e25ed5970fcd4
Nekesa Mercy <kesamercy@gmail.com>
2020-06-12 09:19:28 -0400

    #46 redesign of UI for index page

--- index.php
+++ index.php
@@ -65,13 +65,13 @@ $result = $conn->query($sql);
 
         </div>
     </div>
-    
+
     <br><br><br><br><br><br>
 
     <div class="display-items before-login-bkgd">
-    
-    <h1>Our Packages</h1>
-    <?php
+
+        <h1>Our Packages</h1>
+        <?php
 
 if ($result->num_rows > 0) {
 
@@ -115,95 +115,112 @@ if ($result->num_rows > 0) {
 }
 ?>
 
-</div>
-
-<div class="banner-how-it-works" id="how-it-works">
-
-<div id="content">
-        <div id="left">
-            <div class="slogan">
-
-                <h2>How it works</h2>
-
-                <p>At the beginning of each week we send out an email letting you know what is being harvested by our farmers for the current week’s Farmbox.  You then can go into your account and customize your Farmbox with up to 5 substitutions, per Farmbox. You then can add Artisanal items, and other grocery items we offer to come with your Farmbox delivery.  The cut off is Wednesday at 12PM EST to skip or cancel an order for the next week. The cut off Ito make subs is Friday at 10am EST. We then deliver your Farmbox to your door in a box that is Eco friendly! Please be sure to recycle your boxes and packing material.</p>
-
-<ul>
-    <li>we get our fresh produce from our organic farms</li>
-    <li>You create an account and choose your delivery frequency</li>
-    <li>select your box size</li>
-    <li>We then deliver our simpliy organic products to your door step </li>
-</ul>
-
-<p>
-We do not charge a fee for registration or membership! </p>
-
-<p>
-
-Minimum order is $38.95 and we have a $5.98 handling and packing fee. All of our boxes and packing material are biodegradable / recyclable.
-
-How It Works:
-
-We use UPS for our delivery needs and provide all customers with tracking information. Unfortunatley we are unable to deliver to PO Boxes. We use insulation that is biodegradable and ice packs to keep your box at the perfect temperature and insure safe delivery.
-
-Delivery Schedule
-
-We deliver on different days to different zip codes.  Please input your zip code to find out your delivery day. Once the box leaves us it is in the hands of UPS.  Your credit card will be charged on Friday at 10am EST. We have a cut off because we must tell our Farmers and Artisans what we need for each delivery. Don’t worry, if you miss this week’s cut off, be sure to still create an account so you are on our email list for the following week's harvest!  
-
-*Please note: Delivery delay can be at the hands of Mother Nature due to weather, or traffic delays, once the order leaves us it is in the hands of UPS. All damage reports must be made to hello@farmboxdirect.com within 24 hours of receiving your order and pictures may be requested*   
-</p>
-
-
-            </div>
-
-        </div>
-
-        <div id="right">
-            <div class="loggedin-image">
-                <button>icon / image</button>
-            </div>
-
-        </div>
     </div>
 
-    
+    <div class="banner-how-it-works" id="how-it-works">
 
-</div>
+        <div id="content">
+            <div id="left">
+                <div class="content-how-it-works">
 
-<div class="banner-delivery" id="where-we-deliver">
+                    <h2>How it works</h2>
 
-<div id="content">
-        <div id="left">
-            <div class="slogan">
+                    <p>At the beginning of each week we send out an email letting you know what is being harvested by
+                        our farmers for the current week’s Farmbox. You then can go into your account and customize your
+                        Farmbox with up to 5 substitutions, per Farmbox. You then can add Artisanal items, and other
+                        grocery items we offer to come with your Farmbox delivery. The cut off is Wednesday at 12PM EST
+                        to skip or cancel an order for the next week. The cut off Ito make subs is Friday at 10am EST.
+                        We then deliver your Farmbox to your door in a box that is Eco friendly! Please be sure to
+                        recycle your boxes and packing material.</p>
 
-                <h2>Where we deliver</h2>
 
-                <p>residential areas and offices around kampala and Mbale</p>
 
+                    <p>
+                        We do not charge a fee for registration or membership! </p>
+
+                    <p>
+
+                        Minimum order is $38.95 and we have a $5.98 handling and packing fee. All of our boxes and
+                        packing material are biodegradable / recyclable.
+
+                        How It Works:
+
+                        We use UPS for our delivery needs and provide all customers with tracking information.
+                        Unfortunatley we are unable to deliver to PO Boxes. We use insulation that is biodegradable and
+                        ice packs to keep your box at the perfect temperature and insure safe delivery.
+
+                        Delivery Schedule
+
+                        We deliver on different days to different zip codes. Please input your zip code to find out your
+                        delivery day. Once the box leaves us it is in the hands of UPS. Your credit card will be charged
+                        on Friday at 10am EST. We have a cut off because we must tell our Farmers and Artisans what we
+                        need for each delivery. Don’t worry, if you miss this week’s cut off, be sure to still create an
+                        account so you are on our email list for the following week's harvest!
+
+                        *Please note: Delivery delay can be at the hands of Mother Nature due to weather, or traffic
+                        delays, once the order leaves us it is in the hands of UPS. All damage reports must be made to
+                        hello@farmboxdirect.com within 24 hours of receiving your order and pictures may be requested*
+                    </p>
+
+
+                </div>
 
             </div>
 
-        </div>
+            <div id="right">
+                <div class="steps-how-it-works">
 
-        <div id="right">
-            <div class="loggedin-image">
-                <button>Delivery image</button>
+                    <ul>
+                        <li>we get our fresh produce from our organic farms</li>
+                        <li>You create an account and choose your delivery frequency</li>
+                        <li>select your box size</li>
+                        <li>We then deliver our simpliy organic products to your door step </li>
+                    </ul>
+                    <button>icon / image</button>
+                </div>
+
             </div>
-
         </div>
+
+
+
     </div>
 
-    
+    <div class="banner-delivery" id="where-we-deliver">
 
-</div>
+        <div id="content">
+            <div id="left">
+                <div class="slogan">
 
-<div class="footer">
-    <a href="contact.php">Need help?</a>
-    <a href="subscribe.php">subscribe</a>
-    <a href="about.php">About us</a>
+                    <h2>Where we deliver</h2>
 
-    <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+                    <p>residential areas and offices around kampala and Mbale</p>
 
-</div>
+
+                </div>
+
+            </div>
+
+            <div id="right">
+                <div class="loggedin-image">
+                    <button>Delivery image</button>
+                </div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+    <div class="footer">
+        <a href="contact.php">Need help?</a>
+        <a href="subscribe.php">subscribe</a>
+        <a href="about.php">About us</a>
+
+        <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="../index.js"></script>
