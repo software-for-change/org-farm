e1853a7028c978d89acef5abbf95e6d37d9ffb46
Nekesa Mercy <kesamercy@gmail.com>
2020-07-21 16:11:25 -0400

    modified header links position

--- index.php
+++ index.php
@@ -39,6 +39,7 @@ $result = $conn->query($sql);
                 </div>
 
                 <div class="w3-display-right sub-link"><a href="subscribe.php">Subscribe</a></div>
+                <div class="w3-display-bottomright cotnact-link"><a href="#contact-us">contactus@gmail.com</a></div>
                 <div class="w3-display-bottommiddle w3-hide-small">
                     <div class="vl"></div>
                 </div>
